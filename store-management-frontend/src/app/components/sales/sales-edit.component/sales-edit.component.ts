562e6a42b06784065f2c88a1f816b44ded734868
MrSachin-05 <sachidanandapanigrahi96@gmail.com>
2025-11-04 01:30:10 +0530

    Sales-edit.component.html file is added

---
+++ store-management-frontend/src/app/components/sales/sales-edit.component/sales-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-edit.component',
+  imports: [],
+  templateUrl: './sales-edit.component.html',
+  styleUrl: './sales-edit.component.scss',
+})
+export class SalesEditComponent {
+
+}
